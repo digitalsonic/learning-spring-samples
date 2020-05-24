7b41b28be739bd2a5f9b515d457e3a17a6f341ab
DigitalSonic <digitalsonic.dxf@gmail.com>
2020-05-24 23:22:53 +0800

    第一章示例

---
+++ ch1/helloworld/src/main/java/learning/spring/helloworld/Application.java
@@ -0,0 +1,20 @@
+package learning.spring.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@RequestMapping("/helloworld")
+	public String helloworld() {
+		return "Hello World! Bravo Spring!";
+	}
+}
