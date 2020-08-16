c2780023e922574aa2aff23dfc84227f8dcf7689
DigitalSonic <digitalsonic.dxf@gmail.com>
2020-08-16 21:36:45 +0800

    6.1 MySQL示例

---
+++ ch6/mysql-datasource-demo/src/main/java/learning/spring/DatasourceDemoApplication.java
@@ -0,0 +1,13 @@
+package learning.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatasourceDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatasourceDemoApplication.class, args);
+    }
+
+}
---
+++ ch6/mysql-datasource-demo/src/test/java/learning/spring/DatasourceDemoApplicationTests.java
@@ -0,0 +1,39 @@
+package learning.spring;
+
+import com.zaxxer.hikari.HikariDataSource;
+import com.zaxxer.hikari.pool.HikariProxyConnection;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class DatasourceDemoApplicationTests {
+    @Autowired
+    private ApplicationContext applicationContext;
+    @Value("${spring.datasource.url}")
+    private String jdbcUrl;
+
+    @Test
+    void testDataSource() throws SQLException {
+        assertTrue(applicationContext.containsBean("dataSource"));
+        DataSource dataSource = applicationContext
+                .getBean("dataSource", DataSource.class);
+        assertTrue(dataSource instanceof HikariDataSource);
+
+        HikariDataSource hikari = (HikariDataSource) dataSource;
+        assertEquals(20, hikari.getMaximumPoolSize());
+        assertEquals(10, hikari.getMinimumIdle());
+        assertEquals("com.mysql.cj.jdbc.Driver", hikari.getDriverClassName());
+        assertEquals(jdbcUrl, hikari.getJdbcUrl());
+    }
+}
+
