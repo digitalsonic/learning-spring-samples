ea41cc83e2b2ae4ee5f39d8dd8743e2f20885546
DigitalSonic <digitalsonic.dxf@gmail.com>
2021-01-14 00:49:56 +0800

    10.2的表单示例

---
+++ ch10/binarytea-form-controller/src/main/java/learning/spring/binarytea/WebSecurityConfiguration.java
@@ -0,0 +1,25 @@
+package learning.spring.binarytea;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                    .antMatchers("/menu").permitAll()
+                    .anyRequest().authenticated().and()
+                .formLogin() // 使用表单登录
+                    .defaultSuccessUrl("/order")
+                    .failureUrl("/login")
+                    .loginProcessingUrl("/doLogin")
+                    .usernameParameter("user")
+                    .passwordParameter("pwd").and()
+                .httpBasic(); // 使用HTTP Basic认证
+    }
+}
