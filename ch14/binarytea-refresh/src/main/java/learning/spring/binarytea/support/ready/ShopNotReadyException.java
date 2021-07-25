a06a128ba8ca4b1e0ac8877a1645ab91e362ca75
DigitalSonic <digitalsonic.dxf@gmail.com>
2021-07-25 22:14:09 +0800

    增加基于Zookeeper的几个例子和可刷新配置的例子

---
+++ ch14/binarytea-refresh/src/main/java/learning/spring/binarytea/support/ready/ShopNotReadyException.java
@@ -0,0 +1,7 @@
+package learning.spring.binarytea.support.ready;
+
+public class ShopNotReadyException extends Exception {
+    public ShopNotReadyException(String message) {
+        super(message);
+    }
+}
