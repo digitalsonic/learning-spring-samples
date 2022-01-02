abe028204f4cab2e8dfb7700ea80b6a12949425b
DigitalSonic <digitalsonic.dxf@gmail.com>
2022-01-02 21:24:58 +0800

    调整示例

--- ch16/gateway/src/main/java/learning/spring/gateway/gateway/GatewayApplication.java
+++ ch16/gateway/src/main/java/learning/spring/gateway/gateway/GatewayApplication.java
@@ -2,15 +2,22 @@ package learning.spring.gateway.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class GatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    @Bean
+    public KeyResolver pathKeyResolver() {
+        return exchange -> Mono.just(exchange.getRequest().getPath().toString());
     }
 
 //    @Bean
