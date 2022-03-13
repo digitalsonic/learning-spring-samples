e71e93160ab217eda40a9a93315127952c15a43f
DigitalSonic <digitalsonic.dxf@gmail.com>
2022-03-13 16:41:10 +0800

    统一版本

---
+++ ch10/binarytea-in-memory-auth/src/test/java/learning/spring/binarytea/repository/TeaMakerRepositoryTest.java
@@ -0,0 +1,35 @@
+package learning.spring.binarytea.repository;
+
+import learning.spring.binarytea.model.Order;
+import learning.spring.binarytea.model.TeaMaker;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest(properties = {"data.init.enable=true",
+        "spring.sql.init.mode=never", "spring.jpa.hibernate.ddl-auto=create-drop"})
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class TeaMakerRepositoryTest {
+    @Autowired
+    private TeaMakerRepository makerRepository;
+
+    @Test
+    @Transactional
+    public void testFindById() {
+        TeaMaker maker = makerRepository.getById(1L);
+        assertNotNull(maker);
+        assertEquals("LiLei", maker.getName());
+
+        List<Order> list = maker.getOrders();
+        assertNotNull(list);
+        assertEquals(2, list.size());
+    }
+}
