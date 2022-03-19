c3fbecb16078b9c016ca1e8eb543213acb8790d6
DigitalSonic <digitalsonic.dxf@gmail.com>
2022-03-19 19:19:33 +0800

    调整SQL

--- ch10/binarytea-csrf/src/main/resources/schema.sql
+++ ch10/binarytea-csrf/src/main/resources/schema.sql
@@ -1,7 +1,7 @@
-drop table if exists t_menu;
-drop table if exists t_order;
 drop table if exists t_order_item;
+drop table if exists t_order;
 drop table if exists t_tea_maker;
+drop table if exists t_menu;
 drop table if exists users;
 drop table if exists authorities;
 drop table if exists persistent_logins;
