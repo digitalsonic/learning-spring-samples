9a81b1985570abfa98600faadc933f886cd2405a
DigitalSonic <digitalsonic.dxf@gmail.com>
2022-03-26 17:26:03 +0800

    统一版本

--- ch13/eureka-server/src/main/java/learning/spring/eureka/EurekaServerApplication.java
+++ ch13/eureka-server/src/main/java/learning/spring/eureka/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package learning.spring.eureka.eureka;
+package learning.spring.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- ch13/eureka-server/src/test/java/learning/spring/eureka/EurekaServerApplicationTests.java
+++ ch13/eureka-server/src/test/java/learning/spring/eureka/EurekaServerApplicationTests.java
@@ -1,4 +1,4 @@
-package learning.spring.eureka.eureka;
+package learning.spring.eureka;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
