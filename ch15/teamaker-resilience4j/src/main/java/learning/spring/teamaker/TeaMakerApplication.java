c8d360b718acc12aff9979359a5a7528627ff030
DigitalSonic <digitalsonic.dxf@gmail.com>
2022-04-14 22:49:38 +0800

    添加文件，调整文件名大小写

---
+++ ch15/teamaker-resilience4j/src/main/java/learning/spring/teamaker/TeaMakerApplication.java
@@ -0,0 +1,13 @@
+package learning.spring.teamaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeaMakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeaMakerApplication.class, args);
+	}
+
+}
