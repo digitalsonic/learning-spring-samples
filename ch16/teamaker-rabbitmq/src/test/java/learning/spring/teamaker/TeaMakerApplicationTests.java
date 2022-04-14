0a45e5a142d569dabd7d09b89559aac46203c5d3
DigitalSonic <digitalsonic.dxf@gmail.com>
2022-04-14 22:48:24 +0800

    调整文件名大小写

---
+++ ch16/teamaker-rabbitmq/src/test/java/learning/spring/teamaker/TeaMakerApplicationTests.java
@@ -0,0 +1,13 @@
+package learning.spring.teamaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeaMakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
